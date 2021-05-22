efd8343599f4660b40d88e2802eab4ee8f1db5f4
Ivan Chernogorsky <covaxi@gmail.com>
2021-05-22 17:03:17 +0300

    publish.cmd fixes

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,6 @@
 @echo off
 dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
 echo.
-echo Copy publish folder somewhere and add it to path
+mkdir %utils%\todo
+xcopy /e /i /y bin\Release\net5.0\win-x64\publish %utils%\todo
+echo Add %utils%\todo to the path variable
